990d20e683e2d9e139a052d5ad96f380512c89e2
Fussita <hualong.chiang@gmail.com>
2024-09-30 11:58:02 -0400

    init: nest_base

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './_config/env';
+
+async function bootstrap() {
+  const logger = new Logger('Main')
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidUnknownValues: true
+  }))
+
+  await app.listen(envs.port);
+  logger.log(`Application running on the port: ${envs.port}`)
+
+}
+bootstrap();
